3ab619d108ad58d1296fe10a1c8737c84635f1d9
jaiganesh707 <jaiganeshjkm@gmail.com>
2025-09-05 12:08:15 +0530

    Added the UserROle and Added Auth provider added live url

--- src/main/java/com/example/RegisterService/Controller/UserController.java
+++ src/main/java/com/example/RegisterService/Controller/UserController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 import java.util.List;
 @RestController
-@RequestMapping("/user")
+@RequestMapping("api/user")
 public class UserController {
 @Autowired private UserServiceImpl userService;
 @Autowired private AuthTokenFilter authTokenFilter;
