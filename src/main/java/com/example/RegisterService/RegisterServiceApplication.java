045978091cd0c58956c3278745d7446285d4b9e6
jaiganesh707 <jaiganeshjkm@gmail.com>
2025-09-05 13:05:30 +0530

    Added the UserROle and Added Auth provider added live url

--- src/main/java/com/example/RegisterService/RegisterServiceApplication.java
+++ src/main/java/com/example/RegisterService/RegisterServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -16,15 +17,19 @@ public class 	RegisterServiceApplication {
 		SpringApplication.run(RegisterServiceApplication.class, args);
 	}
 	@Bean
-	public CorsFilter corsFilter() {
+	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200","https://register-form-front-end.vercel.app"));
+		corsConfiguration.setAllowedOrigins(List.of(
+				"http://localhost:4200",
+				"https://register-form-front-end.vercel.app"
+		));
 		corsConfiguration.setAllowedHeaders(List.of("*"));
 		corsConfiguration.setExposedHeaders(List.of("*"));
-		corsConfiguration.setAllowedMethods(List.of("*"));
-		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
-		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
-		return new CorsFilter(urlBasedCorsConfigurationSource);
+		corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfiguration);
+		return source;
 	}
 }
