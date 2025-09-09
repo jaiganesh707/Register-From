0653ff2bbfcfdbd1671a51cb3323aba87ca3020f
jaiganesh707 <jaiganeshjkm@gmail.com>
2025-09-09 16:00:19 +0530

    Added the UserROle and Added Auth provider added live url

--- src/main/java/com/example/RegisterService/Controller/AuthController.java
+++ src/main/java/com/example/RegisterService/Controller/AuthController.java
@@ -31,8 +31,6 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import javax.management.relation.Role;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
